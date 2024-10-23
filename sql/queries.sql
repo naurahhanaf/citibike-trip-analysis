9f30cfbc904ddaa71939909255c54d64365bf702
Naurahhana Firdaustsabita <naurahhanaffpp@gmail.com>
2024-10-23 17:26:13 +0700

    Create queries.sql

---
+++ sql/queries.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT
+  *
+FROM
+  bigquery-public-data.new_york_citibike.citibike_trips
+WHERE
+  tripduration < 86400 AND
+  starttime IS NOT NULL AND
+  birth_year IS NOT NULL AND
+  gender != 'unknown' AND
+  EXTRACT(YEAR FROM starttime) = 2017
+ORDER BY 
+  starttime ASC;
